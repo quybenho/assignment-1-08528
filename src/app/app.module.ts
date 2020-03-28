ebc77c2edc28000f9f89706ead7643974043ddc6
vanlaanh <quyndpph08528@fpt.edu.vn>
2020-03-28 16:40:17 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
